e42aa37c3ddbe8498abe997da42da75bf651ca54
sdhanrajaxioned <dhanrajs@axioned.com>
2022-05-24 17:37:46 +0530

    added banner slider

--- assets/vendor/script.js
+++ assets/vendor/script.js
@@ -4,13 +4,26 @@ $(document).ready(function() {
     $(this).toggleClass('active');
     $('.nav').toggleClass('active');
     $('body').toggleClass('hidden');
+    $('.slick-prev').toggleClass('active');
   })
 
   //go down btn
   $('.go-down-btn').click(function(){
     var nextSection = $(this).closest('section').next('section');
-    $('html, body').animate({
-      scrollTop: $(nextSection).offset().top
-    }, 1500);
-});
+    $('html, body').animate( { scrollTop: $(nextSection).offset().top }, 1500);
+  });
+
+  //slick slider
+  function videoSlider(sliderHolder, $dots, $arrows, $slideToShow, $slidesToScroll) {
+    sliderHolder.slick({
+      dots: $dots,
+      arrows: $arrows,
+      infinite: true,
+      speed: 400,
+      slidesToShow: $slideToShow,
+      slidesToScroll: $slidesToScroll,
+    });
+  }
+
+  videoSlider($('.banner-slider'), false, true, 1, 1);  
 });
